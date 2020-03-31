b6b799d1ebfce214f398fd55d4919dfc9d90d5de
Hasnain Ali <beinghasnain16@gmail.com>
2020-03-31 17:03:14 +0500

    Learning Python

--- iterators_and_generators.py
+++ iterators_and_generators.py
@@ -27,3 +27,7 @@ def chunker(iterable, size):
 
 for chunk in chunker(range(25), 4):
     print(list(chunk))
+
+sq_list = [x**2 for x in range(10)]  # this produces a list of squares
+
+sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
