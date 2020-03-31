08136d1af541957bccc7e9ba051794ea2d5bfd6d
Hasnain Ali <beinghasnain16@gmail.com>
2020-03-31 16:51:36 +0500

    Learning Python

--- iterators_and_generators.py
+++ iterators_and_generators.py
@@ -6,3 +6,15 @@ def my_range(x):
 for y in my_range(8):
 	print(y)
 	
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    i = 0
+    while i < len(iterable):
+        yield start, iterable[i]
+        i += 1
+        start += 1
+
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
